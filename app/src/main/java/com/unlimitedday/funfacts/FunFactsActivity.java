4ca2ffceff6c47ba2c85338d7d5d68d0a8f2c675
Matt Day <mday100@live.com>
2014-12-25 14:32:36 -0500

    added randomGenerator to onClickListener on button

--- app/src/main/java/com/unlimitedday/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/unlimitedday/funfacts/FunFactsActivity.java
@@ -1,12 +1,13 @@
 package com.unlimitedday.funfacts;
 
+import java.util.Random;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 
 public class FunFactsActivity extends ActionBarActivity {
 
@@ -16,8 +17,23 @@ public class FunFactsActivity extends ActionBarActivity {
         setContentView(R.layout.activity_fun_facts);
 
         // Declare our View variables and assign them the Views from the layout file
-        TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The button was clicked, so update the fact label with a new fact
+                String fact = "";
+                // Randomly select a fact
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(3);
+                fact = randomNumber + "";
+                // Update the label with our dynamic fact
+
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
 
